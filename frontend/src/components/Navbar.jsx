2773f48a009c8d8927cf0280924a87a37e4ba338
Jacava-vscode <dev.vs.code.168@gmail.com>
2025-11-13 20:27:06 +0700

    fix(access): show all navigation links, remove login/signup/logout from navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,38 +1,22 @@
 import { Link, NavLink } from 'react-router-dom'
-import { useAuth } from '../hooks/useAuth'
-
-const Navbar = () => {
-  const { user, logout } = useAuth()
-
-  return (
-    <header className="navbar">
-      <div className="navbar__content">
-        <Link to="/" className="navbar__brand">
-          Game Quiz
-        </Link>
-        <nav className="navbar__links">
-          <NavLink to="/" end>Home</NavLink>
-          <NavLink to="/play">Play Quiz</NavLink>
-          <NavLink to="/leaderboard">Leaderboard</NavLink>
-          {user && <NavLink to="/profile">Profile</NavLink>}
-          {user?.roles?.includes('admin') && <NavLink to="/admin">Admin</NavLink>}
-        </nav>
-        <div className="navbar__actions">
-          {!user && (
-            <>
-              <Link className="btn btn--ghost" to="/login">Login</Link>
-              <Link className="btn" to="/signup">Sign Up</Link>
-            </>
-          )}
-          {user && (
-            <button className="btn" onClick={logout} type="button">
-              Logout
-            </button>
-          )}
-        </div>
+const Navbar = () => (
+  <header className="navbar">
+    <div className="navbar__content">
+      <Link to="/" className="navbar__brand">
+        Game Quiz
+      </Link>
+      <nav className="navbar__links">
+        <NavLink to="/" end>Home</NavLink>
+        <NavLink to="/play">Play Quiz</NavLink>
+        <NavLink to="/leaderboard">Leaderboard</NavLink>
+        <NavLink to="/profile">Profile</NavLink>
+        <NavLink to="/admin">Admin</NavLink>
+      </nav>
+      <div className="navbar__actions">
+        {/* All access: no login/signup/logout buttons */}
       </div>
-    </header>
-  )
-}
+    </div>
+  </header>
+)
 
 export default Navbar
